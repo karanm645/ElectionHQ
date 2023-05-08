58c1cb6567bfbcfc7ddfd22c77e5ae008566144f
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-05-08 16:36:52 -0500

    Establish service to representatives

--- spec/request/api/v1/representatives_request_spec.rb
+++ spec/request/api/v1/representatives_request_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Api::V1::RepresentativesController, :type => :controller do
   describe "Api::V1::RepresentativesController" do 
     it 'returns a successful response' do 
       get :index
-      
       expect(response).to be_successful
     end 
   end 
